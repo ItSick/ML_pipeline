630e6e41dfafea1af8e2510cbf295d91892932b5
ItSick <salesmen1@gmail.com>
2025-04-14 12:59:00 +0300

    Update paths.py

--- paths.py
+++ paths.py
@@ -1,7 +1,7 @@
 datasets = {
-    "creditcard":r"C:\Users\itzik\AI Course\datasets\creditcard.csv",
-    "telco_customer_churn":r"C:\Users\itzik\AI Course\datasets\Telco-Customer-Churn.csv",
-    "beverage_sales":r"C:\Users\itzik\AI Course\datasets\synthetic_beverage_sales_data.csv",
-    "air_quality":r"C:\Users\itzik\AI Course\datasets\updated_air_quality_data.csv",
-    "titanic":r"C:\Users\itzik\AI Course\datasets\Titanic/titanic.csv"
+    "creditcard":r".\datasets\creditcard.csv",
+    "telco_customer_churn":r".\datasets\Telco-Customer-Churn.csv",
+    "beverage_sales":r".\datasets\synthetic_beverage_sales_data.csv",
+    "air_quality":r".\datasets\updated_air_quality_data.csv",
+    "titanic":r".\datasets\Titanic\titanic.csv"
 }
